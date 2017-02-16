6554980b70c4a33501295322e7d563b0b2e9201c
Yauheni_Artsiukh <y.a@mail.com>
2017-02-16 16:44:45 +0300

    2/16/2017 4.44 pm dev branch @DataProvider releasing

---
+++ src/test/java/com/epam/page_object/test_data/TestInput.java
@@ -0,0 +1,11 @@
+package com.epam.page_object.test_data;
+
+
+public class TestInput {
+    public String mailBoxUrl = "https://mail.yandex.by/";
+    public String login = "testtask28";
+    public String password = "testtask28testtask28";
+    public String to = "emartu@yandex.ru";
+    public String subject = "sent via WebDriver";
+    public String body = "testtask28testtask28";
+}
